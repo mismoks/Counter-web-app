6f3d5b6b8d76380272bec570959e52469443beee
Mokkshada <mishrimoks@gmail.com>
2023-10-27 17:14:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const[counter,setcounter]=React.useState(0);
+    return (
+        <div id='parent'>
+            <h1>{counter}</h1>
+            <div>
+                <button onClick={()=>{setcounter(counter+1)}}>Increment</button>
+                <button onClick={()=>{setcounter(counter-1)}}>Decrement</button>
+            </div>
+        </div>
   );
 }
 
